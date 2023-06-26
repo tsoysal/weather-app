1dd623b16a6fb1f6a62adca9f5097012cc8a7ee6
Tahaberk SOYSAL <berksysl.g@gmail.com>
2023-06-26 22:57:58 +0300

    UI created and Api configured

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const nightTemperatura = document.getElementById("temperatura");
+const temp = document.querySelectorAll(".temp");
+const currLocation = document.querySelectorAll(".location");
+const others = document.querySelectorAll(".others");
+const day = document.querySelector(".dayContainer");
+const night = document.querySelector(".nightContainer")
 
 function success(position) {
     const currLatitude = position.coords.latitude;
     const currLongitude = position.coords.longitude;
     async function getWeather() {
-        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${currLatitude}&longitude=${currLongitude}&current_weather=true&timezone=auto`);
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3a98a1009a63448aa50164401232606&q=${currLatitude},${currLongitude}&aqi=no`);
         const data = await response.json();
-        const temperature = await data.current_weather.temperature;
-        
+        if(data.current.is_day){
+            night.classList.remove("active");
+            day.classList.add("active");
+        }
+        else {
+            day.classList.remove("active");
+            night.classList.add("active");
+        }
+        currLocation.forEach(element => element.innerText = data.location.name)
+        temp.forEach(element => element.innerText = data.current.temp_c);
+        others[0].innerText = data.current.wind_kph;
+        others[1].innerText = data.current.humidity;
+        others[2].innerText = data.current.precip_mm;
+        others[3].innerHTML = others[0].innerHTML;
+        others[4].innerHTML = others[1].innerHTML;
+        others[5].innerHTML = others[2].innerHTML;
     }
     getWeather();
 }
